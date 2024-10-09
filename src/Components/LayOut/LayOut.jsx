4ae9b1ac146acd7ae1c4ba10504bcf6b3e75fdc4
wanditt <79541813+wanditt@users.noreply.github.com>
2024-10-09 19:29:45 -0400

    Update

--- src/Components/LayOut/LayOut.jsx
+++ src/Components/LayOut/LayOut.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Header from "../Header/Header";
 
 function LayOut({ children }) {
-  return(
-  <div>
-    <Header />
-    {children}
-  </div>)
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 }
 
 export default LayOut;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LayOut from "../../Components/LayOut/Layout";
+import LayOut from "../../Components/LayOut/LayOut";
 import Carousel from "../../Components/Carousel/Carousel";
 import Catagory from "../../Components/Catagory/Catagory";
 import Product from "../../Components/Product/Product";
